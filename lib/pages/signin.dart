2ea3506eec98fe902739a4ba3b78ad26e68f38d1
HammadBullah <hammadbullah@gmail.com>
2025-01-09 13:17:45 +0530

    first

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -22,141 +22,159 @@ class SignInPage extends StatelessWidget {
     final theme = Theme.of(context);
     final isDarkMode = theme.brightness == Brightness.dark;
     double screenHeight = MediaQuery.of(context).size.height;
+    final Brightness platformBrightness = MediaQuery.of(context).platformBrightness;
 
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: isDarkMode
-                ? [Colors.black87, Colors.black]
-                : [Colors.deepOrangeAccent, Colors.black],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: isDarkMode
+                  ? [Colors.deepOrangeAccent, Colors.black]
+                  : [Colors.deepOrangeAccent, Colors.black],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: screenHeight * 0.1),
-            Text(
-              "Pluck n Pay",
-              style: theme.textTheme.bodyLarge?.copyWith(
-                fontSize: 70,
-                fontWeight: FontWeight.w900,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "Shop Smart, Save Big!",
-              style: theme.textTheme.bodyMedium?.copyWith(
-                color: Colors.white54,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: screenHeight * 0.05),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(30.0, 0, 30, 10),
-                decoration: BoxDecoration(
-                  color: theme.scaffoldBackgroundColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        prefixIcon: Icon(Icons.email, color: theme.primaryColor),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: theme.primaryColor),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: theme.primaryColor),
-                        ),
+                    SizedBox(height: screenHeight * 0.1),
+                    Text(
+                      "Pluck n Pay",
+                      style: theme.textTheme.bodyLarge?.copyWith(
+                        fontSize: 70,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    SizedBox(height: 10),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        prefixIcon: Icon(Icons.lock, color: theme.primaryColor),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: theme.primaryColor),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(color: theme.primaryColor),
-                        ),
+                    Text(
+                      "Shop Smart, Save Big!",
+                      style: theme.textTheme.bodyMedium?.copyWith(
+                        color: Colors.white54,
                       ),
-                      obscureText: true,
+                      textAlign: TextAlign.center,
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomePage()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: theme.primaryColor,
-                        padding: EdgeInsets.symmetric(vertical: 20),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        minimumSize: Size(double.infinity, 50),
-                      ),
-                      child: Text(
-                        "Sign In",
-                        style: theme.textTheme.bodyLarge?.copyWith(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).push(_createRoute());
-                      },
-                      child: Text(
-                        "Don't have an account? Sign Up",
-                        style: theme.textTheme.bodyMedium?.copyWith(color: theme.primaryColor),
-                      ),
-                    ),
-                    Divider(height: 30),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        // Add Google login logic
-                      },
-                      icon: Image.asset(
-                        'assets/images/glogo-removebg-preview.png',
-                        height: 24,
-                        width: 24,
-                      ),
-                      label: Text("Sign In with Google"),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: theme.primaryColor,
-                        backgroundColor: theme.scaffoldBackgroundColor,
-                        padding: EdgeInsets.symmetric(vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          side: BorderSide(color: theme.primaryColor),
-                        ),
-                        minimumSize: Size(double.infinity, 50),
-                      ),
-                    ),
+                    SizedBox(height: screenHeight * 0.05),
                   ],
                 ),
               ),
-            ),
-          ],
+              SliverFillRemaining(
+                hasScrollBody: true,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(30.0, 30, 30, 10),
+                  decoration: BoxDecoration(
+                    color: platformBrightness == Brightness.dark
+                        ? Colors.grey[900] // Dark background for dark mode
+                        : Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email, color: theme.primaryColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.lock, color: theme.primaryColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => HomePage()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: theme.primaryColor,
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            minimumSize: Size(double.infinity, 50),
+                          ),
+                          child: Text(
+                            "Sign In",
+                            style: theme.textTheme.bodyLarge?.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).push(_createRoute());
+                          },
+                          child: Text(
+                            "Don't have an account? Sign Up",
+                            style: theme.textTheme.bodyMedium?.copyWith(
+                              color: platformBrightness == Brightness.dark
+                                  ? Colors.white70
+                                  : Colors.black87,
+                            ),
+                          ),
+                        ),
+                        Divider(height: 40),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            // Add Google login logic
+                          },
+                          icon: Image.asset(
+                            'assets/images/glogo-removebg-preview.png',
+                            height: 24,
+                            width: 24,
+                          ),
+                          label: Text("Sign In with Google"),
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: theme.primaryColor,
+                            backgroundColor: Colors.white,
+                            padding: EdgeInsets.symmetric(vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(color: theme.primaryColor),
+                            ),
+                            minimumSize: Size(double.infinity, 50),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -5,160 +5,188 @@ import 'homepage.dart';
 class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Detect the platform's brightness (light or dark mode)
     final Brightness platformBrightness = MediaQuery.of(context).platformBrightness;
+    final theme = Theme.of(context);
+    final isDarkMode = theme.brightness == Brightness.dark;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign Up'),
-        actions: [
-          // Optionally, you can include other actions here if needed
-        ],
-      ),
-      body: Container(
-        color: Theme.of(context).scaffoldBackgroundColor, // Use the theme's background color
-        child: Column(
-          children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-            Text(
-              "Pluck n Pay",
-              style: TextStyle(
-                fontFamily: 'LeagueSpartan',
-                fontSize: 70,
-                fontWeight: FontWeight.w900,
-                color: Theme.of(context).primaryColor, // Primary color from the theme
-              ),
-              textAlign: TextAlign.center,
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: isDarkMode
+                  ? [Colors.deepOrangeAccent, Colors.black]
+                  : [Colors.deepOrangeAccent, Colors.black],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
-            Text(
-              "Shop Smart, Save Big!",
-              style: TextStyle(
-                fontFamily: 'LeagueSpartan',
-                fontSize: 18,
-                color: platformBrightness == Brightness.dark ? Colors.grey[400] : Colors.grey[700], // Conditional color based on system theme
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(30.0, 0, 30, 10),
-                decoration: BoxDecoration(
-                  color: platformBrightness == Brightness.dark
-                      ? Colors.grey[900] // Dark background for dark mode
-                      : Colors.white, // Light background for light mode
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
+          ),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        prefixIcon: Icon(Icons.person),
-                        labelStyle: TextStyle(
-                          color: platformBrightness == Brightness.dark
-                              ? Colors.white70 // Light label for dark mode
-                              : Colors.black87, // Dark label for light mode
-                        ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                    Text(
+                      "Pluck n Pay",
+                      style: TextStyle(
+                        fontFamily: 'LeagueSpartan',
+                        fontSize: 70,
+                        fontWeight: FontWeight.w900,
+                        color: Theme.of(context).scaffoldBackgroundColor,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    SizedBox(height: 10),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        prefixIcon: Icon(Icons.email),
-                        labelStyle: TextStyle(
-                          color: platformBrightness == Brightness.dark
-                              ? Colors.white70
-                              : Colors.black87,
-                        ),
+                    Text(
+                      "Shop Smart, Save Big!",
+                      style: TextStyle(
+                        fontFamily: 'LeagueSpartan',
+                        fontSize: 18,
+                        color: platformBrightness == Brightness.dark ? Colors.grey[400] : Colors.white60,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    SizedBox(height: 10),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        prefixIcon: Icon(Icons.lock),
-                        labelStyle: TextStyle(
-                          color: platformBrightness == Brightness.dark
-                              ? Colors.white70
-                              : Colors.black87,
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Confirm Password',
-                        prefixIcon: Icon(Icons.lock),
-                        labelStyle: TextStyle(
-                          color: platformBrightness == Brightness.dark
-                              ? Colors.white70
-                              : Colors.black87,
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomePage()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 20),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        minimumSize: Size(double.infinity, 50),
-                      ),
-                      child: Text("Sign Up", style: TextStyle(fontSize: 18)),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).push(_createRoute());
-                      },
-                      child: Text(
-                        "Already have an account? Sign In",
-                        style: TextStyle(
-                          color: platformBrightness == Brightness.dark
-                              ? Colors.white70
-                              : Colors.black87,
-                        ),
-                      ),
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        // Add Google login logic
-                      },
-                      icon: Image.asset(
-                        'assets/images/glogo-removebg-preview.png',
-                        height: 24,
-                        width: 24,
-                      ),
-                      label: Text("Sign Up with Google"),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.deepOrangeAccent,
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          side: BorderSide(color: Colors.deepOrangeAccent),
-                        ),
-                        minimumSize: Size(double.infinity, 50),
-                      ),
-                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.05),
                   ],
                 ),
               ),
-            ),
-          ],
+              SliverFillRemaining(
+                hasScrollBody: true,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(30.0, 30, 30, 10),
+                  decoration: BoxDecoration(
+                    color: platformBrightness == Brightness.dark
+                        ? Colors.grey[900]
+                        : Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            prefixIcon: Icon(Icons.person, color: theme.primaryColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email, color: theme.primaryColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.lock, color: theme.primaryColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 10),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Confirm Password',
+                            prefixIcon: Icon(Icons.lock, color: theme.primaryColor),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: theme.primaryColor),
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => HomePage()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            minimumSize: Size(double.infinity, 50),
+                          ),
+                          child: Text(
+                            "Sign Up",
+                            style: theme.textTheme.bodyLarge?.copyWith(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).push(_createRoute());
+                          },
+                          child: Text(
+                            "Already have an account? Sign In",
+                            style: theme.textTheme.bodyMedium?.copyWith(
+                              color: platformBrightness == Brightness.dark
+                                  ? Colors.white70
+                                  : Colors.black87,
+                            ),
+                          ),
+                        ),
+                        Divider(height: 40),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            // Add Google login logic
+                          },
+                          icon: Image.asset(
+                            'assets/images/glogo-removebg-preview.png',
+                            height: 24,
+                            width: 24,
+                          ),
+                          label: Text("Sign Up with Google"),
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.deepOrangeAccent,
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(color: Colors.deepOrangeAccent),
+                            ),
+                            minimumSize: Size(double.infinity, 50),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
